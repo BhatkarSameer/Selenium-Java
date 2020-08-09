d4fd38144f7543e4a5f33557038611d3fd6f4081
BhatkarSameer <bhatkarsam1993@gmail.com>
2020-08-09 14:06:01 +0530

    POM_SeleniumFramework - Framework blank from Develop

---
+++ POM_SeleniumFramework/src/main/java/org/selenium/pomFramework/pageFactory/homePage.java
@@ -0,0 +1,37 @@
+package org.selenium.pomFramework.pageFactory;
+
+public class homePage {
+
+    public void homePageHeader(){
+
+    }
+
+    public void customerSection(){
+
+    }
+
+    public void supportSection(){
+
+    }
+
+    public void salesSection(){
+
+    }
+
+    public void loginSection(){
+
+    }
+
+    public void signUpSection(){
+
+    }
+
+    public void translateSection(){
+
+    }
+
+    public void searchSection(){
+
+    }
+
+}
